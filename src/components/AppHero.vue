8d312d579dafde0fc3f75fea2b8ddf97e6fd01f0
daniloeberli <danilo.eberli@gmail.com>
2023-03-22 15:28:24 +0100

    feat: AppHero component

---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            message: 'Jumbo: Lorem ipsum dolor sit amet, consectetur'
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<style scoped>
+p {
+    font-size: 2rem;
+}
+
+div {
+    background-color: coral;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    padding: 40px;
+}
+</style>
